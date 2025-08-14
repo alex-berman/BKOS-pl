d8c5f76ea17cb773afbde69821b607f33273eb5b
Alex Berman <alexander.berman@gu.se>
2025-08-14 11:49:48 +0200

    Remove code not needed without blocked tests

--- bkos_host_logic.pl
+++ bkos_host_logic.pl
@@ -3,23 +3,13 @@ response_move(Q, Move) :-
   @P,
 	answer_move(Q, P, Move).
 
-relevant_answer(P, P) :-
-	is_polar_question(P).
+relevant_answer(P, P).
 
-relevant_answer(P, not(P)) :-
-	is_polar_question(P).
-
-is_polar_question(Q) :-
-	\+ is_wh_question(Q).
-
-
-is_wh_question(_^_).
+relevant_answer(P, not(P)).
 
 
 answer_move(Q, P, confirm(P)) :-
-	is_polar_question(P),
 	P = Q.
 
 answer_move(Q, not(P), disconfirm(not(P))) :-
-	is_polar_question(P),
 	P = Q.
