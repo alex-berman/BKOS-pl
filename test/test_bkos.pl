7b47bea20ab266a5bd9409dc888b44c1bdef32e3
Alex Berman <alexander.berman@gu.se>
2025-08-14 11:03:58 +0200

    Add coverage test file for thesis

--- test/test_bkos.pl
+++ test/test_bkos.pl
@@ -1,12 +1,12 @@
 :- ensure_loaded(bkos).
 :- use_module('test/dialog_testing').
 
-run_test(Name) :-
-    run_test('test/dialog_coverage.yml', Name).
-
 :- begin_tests(dialog_coverage).
 
-test(bkos, [forall(get_test(TestAsDict, 'test/dialog_coverage.yml'))]) :-
+test(thesis, [forall(get_test(TestAsDict, 'test/dialog_coverage_thesis.yml'))]) :-
+    run_test_from_dict(TestAsDict).
+
+test(various, [forall(get_test(TestAsDict, 'test/dialog_coverage_various.yml'))]) :-
     run_test_from_dict(TestAsDict).
 
 :- end_tests(dialog_coverage).
