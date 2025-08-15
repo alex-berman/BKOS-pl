24f9ca01587c81f3c9cac7f2926c47ddb8207323
Alex Berman <alexander.berman@gu.se>
2025-08-15 14:03:55 +0200

    Support answering arbitrary wh-questions

--- bkos_host_logic.pl
+++ bkos_host_logic.pl
@@ -14,9 +14,11 @@ relevant_answer(P, not(P)).
 relevant_answer([_, _]^supports(_, X, _), E) :-
 	@supports(E, X, _).
 
+relevant_answer(_^P, P).
+
 
 answer_move(P, P, confirm(P)).
 
 answer_move(P, not(P), disconfirm(not(P))).
 
-answer_move(_^supports(_, _, _), P, assert(P)).
+answer_move(_^_, P, assert(P)).
