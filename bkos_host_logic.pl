aad20bbd73c427a50c1e3ddbec0031f823cad1d0
Alex Berman <alexander.berman@gu.se>
2025-09-04 14:20:00 +0200

    Refactor slightly

--- bkos_host_logic.pl
+++ bkos_host_logic.pl
@@ -87,7 +87,7 @@ answer_move([]>>P, P, confirm(P)).
 
 answer_move([]>>P, not(P), disconfirm(not(P))).
 
-answer_move(_>>_, P, assert(P)).
+answer_move(_, P, assert(P)).
 
 
 select_answers(Q, Candidates, Result) :-
