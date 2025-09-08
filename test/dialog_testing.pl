78208f2f382add79cf59f08a1d9e2061c06bf9ac
Alex Berman <alexander.berman@gu.se>
2025-09-08 11:56:17 +0200

    WIP Replace selection policies with branched testing

--- test/dialog_testing.pl
+++ test/dialog_testing.pl
@@ -27,18 +27,18 @@ run_test_from_dict(Name:Test) :-
             assert_fact(StateID, FactStr))
         ; true
     ),
-    test_turns(StateID, Test, 1),
+    test_turns(StateID, Test.turns, 1),
     !.
 
 
-test_turns(StateID, Test, N) :-
-    length(Test.turns, NumTurns),
+test_turns(StateID, Turns, N) :-
+    length(Turns, NumTurns),
     ( N =< NumTurns ->
-        nth1(N, Test.turns, TurnStr),
-        format('Turn #~w: ~w\n', [N, TurnStr]),
-        test_turn(StateID, Test, TurnStr, NextStateID),
+        nth1(N, Turns, Turn),
+        format('Turn #~w: ~w\n', [N, Turn]),
+        test_turn(StateID, Turn, NextStateID),
         NextN is N + 1,
-        test_turns(NextStateID, Test, NextN)
+        test_turns(NextStateID, Turns, NextN)
     ;
         true
     ).
@@ -54,65 +54,59 @@ assert_fact(StateID, Str) :-
     db_add(StateID, Fact).
 
 
-test_turn(StateID, Test, "S", NextStateID) :-
+test_turn(StateID, Turn, NextStateID) :-
+    get_dict('S', Turn, Content),
     !,
-    get_next_state(StateID, Test, NextStateID),
-    assertion(\+ db_get(NextStateID, utter(_))).
+    test_system_turn(StateID, Content, NextStateID).
 
-test_turn(StateID, Test, TurnStr, NextStateID) :-
-    atom_concat("S ", ExpectedSystemMoveAtom, TurnStr),
+test_turn(StateID, Turn, StateID) :-
+    get_dict('U', Turn, Content),
     !,
-    test_system_turn(StateID, Test, ExpectedSystemMoveAtom, NextStateID).
+    test_user_turn(StateID, Content).
 
-test_turn(StateID, _, TurnStr, StateID) :-
-    atom_concat("U ", InterpretationAtom, TurnStr),
+test_turn(_, Turn, _) :-
+    throw(unsupported_turn_format(Turn)).
+
+
+
+test_system_turn(StateID, ExpectedOutputAsDict, NextStateID) :-
+    is_dict(ExpectedOutputAsDict),
     !,
-    test_user_turn(StateID, InterpretationAtom).
-
-test_turn(_, _, TurnStr, _) :-
-    throw(unsupported_turn_format(TurnStr)).
-
-
-get_next_state(StateID, Test, NextStateID) :-
+    get_dict(one_of, ExpectedOutputAsDict, ExpectedSystemMoveAtoms),
+    findall(Move, (member(Atom, ExpectedSystemMoveAtoms), atom_to_term(Atom, Move, _)), ExpectedSystemMoves),
     apply_rules(StateID, CandidateNextStateIDs),
-    select_next_state(CandidateNextStateIDs, Test, NextStateID).
+    findall(Move, (member(CandidateNextStateID, CandidateNextStateIDs), db_get(CandidateNextStateID, utter(Move))), ActualSystemMoves),
+    assertion(sets_are_structurally_equal(ActualSystemMoves, ExpectedSystemMoves)),
+    CandidateNextStateIDs = [NextStateID|_].
 
-
-select_next_state([], _) :-
-    throw(no_next_state),
-    !.
-
-select_next_state([NextStateID], _, NextStateID) :- !.
-
-select_next_state(CandidateNextStateIDs, Test, NextStateID) :-
-    get_dict(selection_policies, Test, SelectionPolicies),
-    member(Policy, SelectionPolicies),
-    apply_selection_policy(CandidateNextStateIDs, Policy, NextStateID).
-
-select_next_state(CandidateNextStateIDs, _, _) :-
-    throw(ambiguous_next_state(CandidateNextStateIDs)).
-
-
-apply_selection_policy(CandidateNextStateIDs, Policies, NextStateID) :-
-    is_list(Policies),
-    !,
-    member(Policy, Policies),
-    apply_selection_policy(CandidateNextStateIDs, Policy, NextStateID).
-
-apply_selection_policy(CandidateNextStateIDs, PolicyStr, NextStateID) :-
-    term_string(Policy, PolicyStr),
-    Policy = (select_state(CandidateNextStateIDs, NextStateID) :- Condition),
-    Condition,
-    !.
-
-
-test_system_turn(StateID, Test, ExpectedSystemMoveAtom, NextStateID) :-
+test_system_turn(StateID, ExpectedSystemMoveAtom, NextStateID) :-
     atom_to_term(ExpectedSystemMoveAtom, ExpectedSystemMove, _),
-    get_next_state(StateID, Test, NextStateID),
+    apply_rules(StateID, CandidateNextStateIDs),
+    assertion(length(CandidateNextStateIDs, 1)),
+    CandidateNextStateIDs = [NextStateID],
     assertion(db_get(NextStateID, utter(_))),
     db_get(NextStateID, utter(ActualSystemMove)),
     assertion(ActualSystemMove =@= ExpectedSystemMove),
     db_remove(NextStateID, utter(_)).
+
+
+sets_are_structurally_equal(Xs, Ys) :-
+    subset_structural(Xs, Ys),
+    subset_structural(Ys, Xs).
+
+
+subset_structural([], _).
+
+subset_structural([E|Es], Ys) :-
+    memberchk_structural(E, Ys),
+    subset_structural(Es, Ys).
+
+
+memberchk_structural(E, [Y|_]) :-
+    E =@= Y, !.
+
+memberchk_structural(E, [_|Ys]) :-
+    memberchk_structural(E, Ys).
 
 
 test_user_turn(StateID, InterpretationAtom) :-
