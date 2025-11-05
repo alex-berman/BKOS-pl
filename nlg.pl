ca7f22c3824663f4488a49224f3cb2751ffb29be
Alex Berman <alexander.berman@gu.se>
2025-11-05 13:41:27 +0100

    Extend NLG

--- nlg.pl
+++ nlg.pl
@@ -1,7 +1,7 @@
 :- module(nlg, [generate/2]).
 
 
-assert(P) --> proposition(P).
+assert(P) --> proposition(P), glue('.').
 
 proposition(rel_prob(Event, R)) --> [there, is, a, R, probability, that], event(Event).
 proposition(value(P, V)) --> property(P), [is, V].
@@ -12,13 +12,13 @@ proposition(rel_value(back_pain(pat_1), V)) -->
 proposition(rel_value(disability(pat_1), V)) --> [the, patient, has, relatively, V, disability].
 proposition([X]) --> proposition(X).
 proposition([X,Y]) --> proposition(X), [and], proposition(Y).
-proposition([X,Y|Rest]) --> proposition(X), [','], proposition(Y), [and], proposition(Rest).
+proposition([X,Y|Rest]) --> proposition(X), glue(','), proposition(Y), [and], proposition(Rest).
 proposition(supports(A, C, association)) --> ['I', associate], concept(A), [with], concept(C).
 proposition(surgery_recommended(pat_1)) --> [surgery, is, recommended, for, the, patient].
 proposition(not(surgery_recommended(pat_1))) --> [surgery, is, not, recommended, for, the, patient].
 
-confirm(P) --> ['yes,'], proposition(P).
-disconfirm(P) --> ['no,'], proposition(P).
+confirm(P) --> ['yes,'], proposition(P), glue('.').
+disconfirm(P) --> ['no,'], proposition(P), glue('.').
 
 event(satisfied(pat_1)) --> [the, patient, will, be, satisfied, with, surgery].
 
@@ -35,45 +35,45 @@ back_pain_level(high) --> [severe].
 presence_or_absense(false) --> [absense].
 presence_or_absense(true) --> [presence].
 
-signal_resumption --> [also, ','].
+signal_resumption --> [also], glue(',').
 
-infer(A, C) --> [since], proposition(A), [','], proposition(C).
+infer(A, C) --> [since], proposition(A), glue(','), proposition(C), glue('.').
 
-icm(acceptance, negative, lack_knowledge) --> ['I', 'don\'t', have, any, information, about, that].
+icm(acceptance, negative, lack_knowledge) --> ['I', 'don\'t', have, any, information, about, that], glue('.').
+icm(understanding, negative, unresolvable_phrase(P)) -->
+    [sorry], glue(','), ['I', 'don\'t', understand, what,
+    '"'], glue(P), glue('"'), [means, in, this, context], glue('.').
+
+glue(Token) --> ['_glue_', Token].
 
 
 generate(Move, Sentence) :-
     ( is_list(Move) ->
         dcg_sequence(Move, Sequence),
-        phrase(Sequence, Words1)
+        phrase(Sequence, Tokens)
     ;
-        phrase(Move, Words1)
+        phrase(Move, Tokens)
     ),
     !,
-    normalize(Words1, Words),
-    atomic_list_concat(Words, ' ', Raw),
-    capitalize_first(Raw, Capitalized),
-    add_period(Capitalized, Sentence).
+    concat(Tokens, Raw),
+    capitalize_first(Raw, Sentence).
 
 dcg_sequence([X], X).
 dcg_sequence([X|Xs], (X, Rest)) :-
     dcg_sequence(Xs, Rest).
 
-normalize([X, ','|RestIn], [X1|RestOut]) :-
+concat([], '').
+concat([X], X) :- !.
+concat([X,'_glue_'|Xs], Result) :-
     !,
-    atom_concat(X, ',', X1),
-    normalize(RestIn, RestOut).
-
-normalize([], []).
-
-normalize([X|RestIn], [X|RestOut]) :-
-    normalize(RestIn, RestOut).
+    concat(Xs, XsResult),
+    atomic_list_concat([X, XsResult], Result).
+concat([X|Xs], Result) :-
+    concat(Xs, XsResult),
+    atomic_list_concat([X, ' ', XsResult], Result).
 
 capitalize_first(AtomIn, AtomOut) :-
     atom_chars(AtomIn, [First|Rest]),
     char_type(First, to_lower(Lower)),
     char_type(Cap, to_upper(Lower)),
     atom_chars(AtomOut, [Cap|Rest]).
-
-add_period(AtomIn, AtomOut) :-
-    atom_concat(AtomIn, '.', AtomOut).
